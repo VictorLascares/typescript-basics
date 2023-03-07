917346be08c76bbf8d2649a83e425a4857d2ac47
Victor Lascares <vmlascares@gmail.com>
2023-03-07 17:22:14 -0600

    Tipos de datos

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+(function () {
+    let mensaje = 'Hola';
+    let numero = 123;
+    let booleano = true;
+    let hoy = new Date();
+    let cualquierCosa;
+    cualquierCosa = mensaje;
+    cualquierCosa = numero;
+    cualquierCosa = booleano;
+    cualquierCosa = hoy;
+    let spiderman = {
+        nombre: 'Peter',
+        edad: 30
+    };
+})();
